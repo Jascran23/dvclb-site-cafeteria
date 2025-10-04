a61339a44de583f2c30bc37ca2ffd4a00c40977b
Jascran23 <cjean475@gmail.com>
2025-10-04 12:39:13 -0300

    animação no menu mobile

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const btnIconMobile = document.querySelector('.icon-menu-list');
 const menuList = document.getElementById('navbar-mobile');
-
+const navLinks = document.querySelectorAll('.list');
 
 btnIconMobile.addEventListener(('click'), ()=> {
     if(menuList.style.display == 'none'){
         menuList.style.display = 'block';
+        
+    navLinks.forEach((link, index) => {  
+        link.style.animation = `effectTranslate 2s ease forwards ${
+        index / 7 + 0.1
+        }s`;
+    });
+
+    navLinks.style.backgroundColor = 'red';
+     
     }
-    else {menuList.style.display = 'none';}
-    
+    else {
+        menuList.style.display = 'none'; 
+        
+        navLinks.forEach((link, index) => {         
+                link.style = 'none';
+            });
+        }
+
 })
